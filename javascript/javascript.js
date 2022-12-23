f44edb1d333dbb997f50ee050df2a91c60b61f68
donat3 <118883706+donat3@users.noreply.github.com>
2022-12-23 22:01:20 +0100

    edit if-at te javascript regex kushtet

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,8 @@
 function validoInput() {
+
     var regexEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.(com|net)$/;
     var emailUserit = document.getElementById('email').value;
+
     var regexPassword = /^[A-Z]+[A-Za-z0-9!@#$%^&*()_+=-]{7,}[0-9!@#$%^&*()_+=-]/;
     var passUserit = document.getElementById('password').value;
 
@@ -9,59 +11,70 @@ function validoInput() {
         return;
     } if (emailUserit == "") {
         alert("Email eshte shkruar gabim!\n\nEmail nuk duhet te jete i zbrazte!");
+
     } if (regexEmail != true) {
         alert("Email eshte shkruar gabim!\n\nEmail mund te kete shkronja te medha ose te vogla!\nEmail duhet te kete shenjen @\nEmail duhet te perfundoj me .com ose .net");
-    }if (passUserit == "") {
+
+    } if (passUserit == "") {
         alert("Password eshte shkruar gabim!\n\nPassword nuk duhet te jete i zbrazte!");
+
     } if (regexPassword != true) {
         alert("Password eshte shkruar gabim!\n\nPassword duhet te jete i gjate minimum 7 karaktere!\nPasswordi duhet te filloj me shkronje te madhe!");
     }
 }
 
 function validoInputSignup() {
-    var regexEmriU = /^[A-Z][A-Za-z]{1,}$/;
+    var regexEmriU = /^[A-Z]+[A-Za-z]{2,}$/;
     var emriUserit = document.getElementById('emri').value;
-    var regexMbimriU = /^[A-Z][A-Za-z]{1,}$/;
+
+    var regexMbiemriU = /^[A-Z]+[A-Za-z]{2,}$/;
     var mbiemrilUserit = document.getElementById('mbiemri').value;
+
     var regexEmailUserit = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.(com|net)$/;
     var emailUseritS = document.getElementById('emailS').value;
+
     var regexPasswordUserit = /^[A-Z]+[A-Za-z0-9!@#$%^&*()_+=-]{7,}[0-9!@#$%^&*()_+=-]/;
     var passUseritS = document.getElementById('passwordS').value;
+
     var passKUseritS = document.getElementById('passwordK').value;
 
     if (emriUserit == "" && mbiemrilUserit == "" && emailUseritS == "" && passUseritS == "" && passKUseritS == "") {
         alert("Duhet te plotesoni te dhenat!");
         return;
-    } else if (emriUserit == "") {
-        alert("Duhet te shenoni emrin!");
 
-    } else if (mbiemrilUserit == "") {
-        alert("Duhet te shenoni mbiemrin!");
+    } if (emriUserit == "") {
+        alert("Emri eshte shkruar gabim!\nEmri nuk duhet te jete i zbrazte!");
 
-    } else if (emailUseritS == "") {
-        alert("Duhet te shenosh email!");
+    } if (regexEmriU != true) {
+        alert("Emri eshte shkruar gabim!\n\nEmri duhet te filloj me shkronje te madhe!\nEmri duhet te jete minimum 2 karaktere!");
 
-    } else if (passUseritS == "") {
-        alert("Duhet te shenoni passwordin");
+    } if (mbiemrilUserit == "") {
+        alert("Mbiemri eshte shkruar gabim!\nMbiemri nuk duhet te jete i zbrazte!");
 
-    } else if (passKUseritS == "") {
+    } if (regexMbiemriU != true) {
+        alert("Mbiemri eshte shkruar gabim!\n\nMbiemri duhet te filloj me shkronje te madhe!\nMbiemri duhet te jete minimum 2 karaktere!");
+
+    } if (emailUseritS == "") {
+        alert("Email eshte shkruar gabim!\nEmail nuk duhet te jete i zbrazte!");
+
+    } if (regexEmailUserit != true) {
+        alert("Email eshte shkruar gabim!\n\nEmail mund te kete shkronja te medha ose te vogla!\nEmail duhet te kete shenjen @\nEmail duhet te perfundoj me .com ose .net");
+
+    } if (passUseritS == "") {
+        alert("Password eshte shkruar gabim!\n\nPassword nuk duhet te jete i zbrazte!");
+
+    } if (regexPasswordUserit != true) {
+        alert("Password eshte shkruar gabim!\n\nPassword duhet te jete i gjate minimum 7 karaktere!\nPasswordi duhet te filloj me shkronje te madhe!");
+
+    } if (passKUseritS == "") {
         alert("Duhet ta konfirmoni passwordin!");
 
-    } else if (passUseritS.length < 8) {
-        alert("Password-i duhet ti kete se paku 8 karaktere!");
-
-    } else if (passUseritS.search(/[A-Z]/) < 0) {
-        alert("Password-i duhet te kete shkronje te madhe!");
-
-    } else if (passUseritS != passKUseritS) {
-        alert("Password-et nuk perputhen!")
-
-    } else {
-        alert("Jeni regjistruar me sukses!");
+    } if (passUseritS != passKUseritS) {
+        alert("Password-et nuk perputhen!");
     }
 
-
     function validoInputRezervo() {
+
         var dataZ = document.getElementById('dataZ').value;
         var orari = document.getElementById('orari').value;
         var persona = document.getElementById('persona').value;
