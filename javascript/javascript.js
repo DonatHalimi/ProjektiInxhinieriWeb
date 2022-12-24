8707f32bd99c1ed703bedf2a77a5a030a467ff58
donat3 <118883706+donat3@users.noreply.github.com>
2022-12-24 09:35:41 +0100

    edit else if ne if te signup edhe login

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -9,20 +9,20 @@ function validoInput() {
     if (emailUserit == "" && passUserit == "") {
         alert("Duhet te shenosh email dhe password!");
         return;
-    }if (emailUserit == "") {
+    } if (emailUserit == "") {
         alert("Email eshte shkruar gabim!\n\nEmail nuk duhet te jete i zbrazte!");
 
-    }else if (!regexEmail.test(emailUserit)) {
+    } if (!regexEmail.test(emailUserit)) {
         alert("Email eshte shkruar gabim!\n\nEmail duhet te kete vetem shkronja te vogla!\nEmail duhet te kete shenjen @\nEmail duhet te perfundoj me .com ose .net");
 
-    }else if (passUserit == "") {
+    } if (passUserit == "") {
         alert("Password eshte shkruar gabim!\n\nPassword nuk duhet te jete i zbrazte!");
 
-    }else if(!regexPassword.test(passUserit)){
+    } if (!regexPassword.test(passUserit)) {
         alert("Password eshte shkruar gabim!\n\nPassword duhet te jete i gjate minimum 7 karaktere!\nPasswordi duhet te filloj me shkronje te madhe!\nPasswordi duhet te mbaroje me ndonje numer apo shenje");
-    }else{
+    } else {
         alert("Jeni regjistruar me sukses!");
-        window.location.href ="index.html";
+        window.location.href = "index.html";
     }
 }
 
@@ -48,35 +48,36 @@ function validoInputSignup() {
     } if (emriUserit == "") {
         alert("Emri eshte shkruar gabim!\nEmri nuk duhet te jete i zbrazte!");
 
-    }else if (!regexEmriU.test(emriUserit)) {
+    } if (!regexEmriU.test(emriUserit)) {
         alert("Emri eshte shkruar gabim!\n\nEmri duhet te filloj me shkronje te madhe!\nEmri duhet te jete minimum 2 karaktere!");
 
-    }else if (mbiemrilUserit == "") {
+    } if (mbiemrilUserit == "") {
         alert("Mbiemri eshte shkruar gabim!\nMbiemri nuk duhet te jete i zbrazte!");
 
-    }else if (!regexMbiemriU.test(mbiemrilUserit)) {
+    } if (!regexMbiemriU.test(mbiemrilUserit)) {
         alert("Mbiemri eshte shkruar gabim!\n\nMbiemri duhet te filloj me shkronje te madhe!\nMbiemri duhet te jete minimum 2 karaktere!");
 
-    }else if (emailUseritS == "") {
+    } if (emailUseritS == "") {
         alert("Email eshte shkruar gabim!\nEmail nuk duhet te jete i zbrazte!");
 
-    }else if (!regexEmailUserit.test(emailUseritS)) {
+    } if (!regexEmailUserit.test(emailUseritS)) {
         alert("Email eshte shkruar gabim!\n\nEmail mund te kete shkronja te medha ose te vogla!\nEmail duhet te kete shenjen @\nEmail duhet te perfundoj me .com ose .net");
 
-    }else if (passUseritS == "") {
+    } if (passUseritS == "") {
         alert("Password eshte shkruar gabim!\n\nPassword nuk duhet te jete i zbrazte!");
 
-    }else if (!regexPasswordUserit.test(passUseritS)) {
+    } if (!regexPasswordUserit.test(passUseritS)) {
         alert("Password eshte shkruar gabim!\n\nPassword duhet te jete i gjate minimum 7 karaktere!\nPasswordi duhet te filloj me shkronje te madhe!");
 
-    }else if (passKUseritS == "") {
+    } if (passKUseritS == "") {
         alert("Duhet ta konfirmoni passwordin!");
 
-    }else if (passUseritS != passKUseritS) {
+    } if (passUseritS != passKUseritS) {
         alert("Password-et nuk perputhen!");
-    }else{
+
+    } else {
         alert("Jeni regjistruar me sukses");
-        window.location.href("login.html");
+        window.location.href = "login.html";
     }
 
     function validoInputRezervo() {
