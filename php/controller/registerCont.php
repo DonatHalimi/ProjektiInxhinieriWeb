75c720c318f094687aaf2a13cf1f3e43b0609e9b
MalMikullovci <malmikullovci1@gmail.com>
2023-02-20 15:53:52 +0100

    haha

--- php/controller/registerCont.php
+++ php/controller/registerCont.php
@@ -1,6 +1,6 @@
 <?php
 include_once '../repository/userRep.php';
-include_once '../models/RegisterPerdoruesi.php';
+include_once '../models/RegisterPerdoruesin.php';
 
 if ($_SERVER['REQUEST_METHOD']==='POST')
 {
